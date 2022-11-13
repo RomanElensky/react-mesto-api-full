1de74377a38412603e47595ab2b6eb272309128a
Roman Elensky <warrior.394@yandex.ru>
2022-11-13 22:56:01 +0400

    12 5

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://api.mesto.balrok.nomoredomains.icu";
+export const BASE_URL = "https://api.mesto.balrok.nomoredomains.icu";
 
 function checkResponse(res) {
    if (res.ok) {
